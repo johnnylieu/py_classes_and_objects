2eef3f0b92909c8fd2c5558651427a183adb30a7
Johnny Lieu <you@example.com>
2022-02-05 12:55:23 -0800

    added constructor init to make it scalable

--- classes_and_objects_in_python.py
+++ classes_and_objects_in_python.py
@@ -1,4 +1,10 @@
 class Robot:
+    #constructor
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
     def introduce_self(self):
         print("My name is " + self.name) #self is like this in Java
 
