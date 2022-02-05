aaa08c9c61b5c55c4b425d91ddd58a8ddfaf63e1
Johnny Lieu <you@example.com>
2022-02-05 12:56:55 -0800

    re-wrote objects and attributes to work with constructor

--- classes_and_objects_in_python.py
+++ classes_and_objects_in_python.py
@@ -8,16 +8,20 @@ class Robot:
     def introduce_self(self):
         print("My name is " + self.name) #self is like this in Java
 
-#objects and attributes
-r1 = Robot()
-r1.name = "Johnny"
-r1.color = "black"
-r1.weight = 30
+#objects and attributes when used without contstructor
+# r1 = Robot()
+# r1.name = "Johnny"
+# r1.color = "black"
+# r1.weight = 30
 
-r2 = Robot()
-r2.name = "Brian"
-r2.color = "brown"
-r2.weight = 60
+# r2 = Robot()
+# r2.name = "Brian"
+# r2.color = "brown"
+# r2.weight = 60
+
+#objects and attributes when used with contstructor
+r1 = Robot("Johnny", "black", 30)
+r2 = Robot("Brian", "brown", 60)
 
 #runes r1 object in function introduce_self
 r1.introduce_self()
