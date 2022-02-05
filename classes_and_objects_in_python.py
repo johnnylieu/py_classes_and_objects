3f6cb75ed7be305a5ea5027f538e69427685c404
Johnny Lieu <you@example.com>
2022-02-05 12:48:08 -0800

    successfully passed object and attributes to fucntion

--- classes_and_objects_in_python.py
+++ classes_and_objects_in_python.py
@@ -2,7 +2,11 @@ class Robot:
     def introduce_self(self):
         print("My name is " + self.name) #self is like this in Java
 
+#objects and attributes
 r1 = Robot()
 r1.name = "Tom"
 r1.color = "red"
 r1.weight = 30
+
+#runes r1 object in function introduce_self
+r1.introduce_self()
